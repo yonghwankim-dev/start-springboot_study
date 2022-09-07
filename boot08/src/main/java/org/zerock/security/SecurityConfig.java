c9334c8626f85ca4d9adefc3ddfcdaa7f3fc9935
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-09-07 16:07:29 +0900

    [추가] 로그인 정보 설정 추가

--- boot08/src/main/java/org/zerock/security/SecurityConfig.java
+++ boot08/src/main/java/org/zerock/security/SecurityConfig.java
@@ -1,11 +1,15 @@
 package org.zerock.security;
 
 import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Log
@@ -22,8 +26,21 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("manager")
+                .password(passwordEncoder().encode("1234"))
+                .authorities("MANAGER");
+    }
+
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer(){
         return (web)->web.ignoring().antMatchers("/ignore1", "/ignore2");
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }

c23da8795e1494d47bdd2226037a2df265583a50
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-09-07 14:37:45 +0900

    [추가] 회원 리포지토리 추가

---
+++ boot08/src/main/java/org/zerock/persistence/MemberRepository.java
@@ -0,0 +1,8 @@
+package org.zerock.persistence;
+
+import org.springframework.data.repository.CrudRepository;
+import org.zerock.domain.Member;
+
+public interface MemberRepository extends CrudRepository<Member, String> {
+
+}

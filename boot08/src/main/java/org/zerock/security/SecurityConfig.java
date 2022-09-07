2e476a1ff159077ac221386296641387ca0d782e
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-09-07 21:13:38 +0900

    [추가] 커스텀 인증 추가

--- boot08/src/main/java/org/zerock/security/SecurityConfig.java
+++ boot08/src/main/java/org/zerock/security/SecurityConfig.java
@@ -12,9 +12,14 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.sql.DataSource;
+
 @Log
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    ZerockUserService zerockUserService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -33,12 +38,16 @@ public class SecurityConfig {
                 .invalidateHttpSession(true).deleteCookies("JSESSIONID")
                 .permitAll();
 
+        // 커스텀 인증 사용
+        http.userDetailsService(zerockUserService);
+
 
         return http.build();
     }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        메모리 기반 인증매니저 생성
         auth.inMemoryAuthentication()
                 .withUser("manager")
                 .password(passwordEncoder().encode("1234"))
---
+++ boot08/src/main/java/org/zerock/security/ZerockUserService.java
@@ -0,0 +1,16 @@
+package org.zerock.security;
+
+import lombok.extern.java.Log;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@Log
+public class ZerockUserService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}

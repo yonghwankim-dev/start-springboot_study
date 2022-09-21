a9c1458cbd281c1e8c84c81fb85126c9ac413a72
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-09-21 14:57:08 +0900

    [삭제] boot07 삭제

--- boot06/src/main/java/org/zerock/Boot06Application.java
+++ boot06/src/main/java/org/zerock/Boot06Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Boot06Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Boot06Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Boot06Application.class, args);
+    }
 
 }
--- boot06/src/test/java/org/zerock/Boot06ApplicationTests.java
+++ boot06/src/test/java/org/zerock/Boot06ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Boot06ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }

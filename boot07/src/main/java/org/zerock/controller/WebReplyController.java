37c7231499b4bbc7e3e875236dd23bd4027206e7
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-09-21 16:16:06 +0900

    [추가] 댓글 등록 처리 추가

--- boot07/src/main/java/org/zerock/controller/WebReplyController.java
+++ boot07/src/main/java/org/zerock/controller/WebReplyController.java
@@ -1,16 +1,28 @@
 package org.zerock.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.java.Log;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.zerock.domain.WebReply;
 import org.zerock.persistence.WebBoardRepository;
 import org.zerock.persistence.WebReplyRepository;
 
 @RestController
 @RequestMapping("/replies/*")
 @RequiredArgsConstructor
+@Log
 public class WebReplyController {
     private final WebReplyRepository webReplyRepository;
 
+    @PostMapping("/{bno}")
+    public ResponseEntity<Void> addReply(@PathVariable("bno") Long bno, @RequestBody WebReply reply){
+        log.info("addReply.......");
+        log.info("BNO   : " + bno);
+        log.info("REPLY : " + reply);
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 
 }

e33191014b0720f1142243ad6de0e7347f92aad9
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-09-21 13:36:28 +0900

    [추가] 게시물 조회 추가

--- boot06/src/main/java/org/zerock/controller/WebBoardController.java
+++ boot06/src/main/java/org/zerock/controller/WebBoardController.java
@@ -54,4 +54,11 @@ public class WebBoardController {
 
         return "redirect:/boards/list";
     }
+
+    @GetMapping("/view")
+    public void view(Long bno, @ModelAttribute("pageVO") PageVO pageVO, Model model){
+        log.info("BNO : " + bno);
+
+        webBoardService.findById(bno).ifPresent(board->model.addAttribute("board",board));
+    }
 }

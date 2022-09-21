59559f073676d70fe719bef0d952b10f3a8bb7e3
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-09-21 11:44:56 +0900

    [추가] 페이지 이동 처리 추가

--- boot06/src/main/java/org/zerock/controller/WebBoardController.java
+++ boot06/src/main/java/org/zerock/controller/WebBoardController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.zerock.domain.WebBoard;
 import org.zerock.service.WebBoardService;
+import org.zerock.vo.PageMarker;
 import org.zerock.vo.PageVO;
 
 @Controller
@@ -23,10 +24,13 @@ public class WebBoardController {
     @GetMapping("/list")
     public void list(PageVO pageVO, Model model){
         // bno 기준 내림차순, 1페이지, 10개
-        Pageable pageable = pageVO.makePageable(0, "bno");
-        Page<WebBoard> webBoardPage = webBoardService.findAll(webBoardService.makePredicates(null, null), pageable);
-        log.info("/boards/list : " + pageable);
+        Pageable page = pageVO.makePageable(0, "bno");
+        Page<WebBoard> result = webBoardService.findAll(webBoardService.makePredicates(null, null), page);
 
-        model.addAttribute("webBoardPage", webBoardPage);
+        log.info("" + page);
+        log.info("" + result);
+        log.info("TOTAL PAGE NUMBER: " + result.getTotalPages());
+
+        model.addAttribute("result", new PageMarker<>(result));
     }
 }

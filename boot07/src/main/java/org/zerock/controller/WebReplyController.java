1dff4919c0eef98bb1ebc33301e0312269363492
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-09-21 16:22:20 +0900

    [추가] 댓글 등록 추가

--- boot07/src/main/java/org/zerock/controller/WebReplyController.java
+++ boot07/src/main/java/org/zerock/controller/WebReplyController.java
@@ -4,10 +4,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.zerock.domain.WebBoard;
 import org.zerock.domain.WebReply;
 import org.zerock.persistence.WebBoardRepository;
 import org.zerock.persistence.WebReplyRepository;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/replies/*")
@@ -16,13 +20,26 @@ import org.zerock.persistence.WebReplyRepository;
 public class WebReplyController {
     private final WebReplyRepository webReplyRepository;
 
+    @Transactional
     @PostMapping("/{bno}")
-    public ResponseEntity<Void> addReply(@PathVariable("bno") Long bno, @RequestBody WebReply reply){
+    public ResponseEntity<List<WebReply>> addReply(@PathVariable("bno") Long bno, @RequestBody WebReply reply){
         log.info("addReply.......");
         log.info("BNO   : " + bno);
         log.info("REPLY : " + reply);
 
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        WebBoard board = WebBoard.builder()
+                                 .bno(bno)
+                                 .build();
+
+        reply.setBoard(board);
+        webReplyRepository.save(reply);
+        return new ResponseEntity<>(getListByBoard(board), HttpStatus.CREATED);
     }
+
+    private List<WebReply> getListByBoard(WebBoard board) {
+        log.info("getListByBoard..." + board);
+        return webReplyRepository.getRepliesOfBoard(board);
+    }
+
 
 }
--- boot07/src/main/java/org/zerock/persistence/WebReplyRepository.java
+++ boot07/src/main/java/org/zerock/persistence/WebReplyRepository.java
@@ -1,8 +1,15 @@
 package org.zerock.persistence;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.zerock.domain.WebBoard;
 import org.zerock.domain.WebReply;
+
+import java.util.List;
 
 public interface WebReplyRepository extends CrudRepository<WebReply, Long> {
 
+    @Query("SELECT r FROM WebReply r WHERE r.board = :board AND r.rno > 0 ORDER BY r.rno ASC")
+    List<WebReply> getRepliesOfBoard(@Param("board") WebBoard board);
 }
